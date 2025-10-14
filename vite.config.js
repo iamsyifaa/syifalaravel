4c222fca5ae3301fd3de6f7d79c0bf5a4835f70f
iamsyifaa <syifaoktaviani1710@gmail.com>
2025-10-14 10:47:08 +0700

    au ah

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    build: {
-        outDir: "public/build",
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
@@ -13,4 +10,10 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        emptyOutDir: true,
+        minify: "esbuild",
+    },
 });
